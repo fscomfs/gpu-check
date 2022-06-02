6bd84a5de6a86c30728d257b8b3f4313601edb68
ZHOUXIAOFENG\user <1041786715@qq.com>
2022-06-02 12:06:45 +0800

    docker file

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,3 @@
-FROM nvidia/cuda:11.2.0-cudnn8-devel-ubuntu18.04
-
-ENV PYTHON_VERSION 3.7
-RUN apt-get update \
-    && apt-get install -y software-properties-common curl \
-    && apt-get install -y python${PYTHON_VERSION} python${PYTHON_VERSION}-dev python3-apt python3-distutils \
-    # 设置默认python版本链接
-    && update-alternatives --install /usr/bin/python python /usr/bin/python${PYTHON_VERSION} 40 \
-    && update-alternatives --install /usr/bin/python3 python3 /usr/bin/python${PYTHON_VERSION} 40 \
-    && update-alternatives --set python /usr/bin/python${PYTHON_VERSION} \
-    && update-alternatives --set python3 /usr/bin/python${PYTHON_VERSION} \
-    # pip, wheel, setuptools
-    && curl https://bootstrap.pypa.io/get-pip.py -o /usr/local/src/get-pip.py \
-    && python${PYTHON_VERSION} /usr/local/src/get-pip.py \
-
-RUN update-alternatives --install /usr/bin/python python /usr/bin/python${PYTHON_VERSION} 40 \
-    && update-alternatives --install /usr/bin/python3 python3 /usr/bin/python${PYTHON_VERSION} 40 \
-    && update-alternatives --set python /usr/bin/python${PYTHON_VERSION} \
-    && update-alternatives --set python3 /usr/bin/python${PYTHON_VERSION} \
+FROM cvmart-zhuhai-tcr.tencentcloudcr.com/public_images/ubuntu18.04-cuda11.1-cudnn8-devel-train-test-pytorch1.10.0-openvino2021r3-workspace-base:v1.2
 COPY . /home
-RUN cd /home && pip3 -r requirements.txt
+RUN cd /home && pip3 install -r requirements.txt
--- src/main.py
+++ src/main.py
@@ -1,66 +1,92 @@
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify
 import os
 import pynvml
-import time
-import  base64
-os.putenv('NODE_IP','192.168.1.1')
-os.putenv('NODE_NAME','test')
-NODE_IP = "192.168.1.1"
-#NODE_IP = os.getenv('NODE_IP')
-NODE_NAME="test"
-#NODE_NAME = os.getenv('NODE_NAME')
+
+NODE_IP = os.getenv('NODE_IP')
+if NODE_IP == None:
+    NODE_IP = "0.0.0.0"
+NODE_NAME = os.getenv('NODE_NAME')
+if NODE_NAME == None:
+    NODE_NAME = "-----"
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
+
 
 def deviceNvmlInit():
     pynvml.nvmlInit()
     print('Driver Version:', pynvml.nvmlSystemGetDriverVersion())
     deviceCount = pynvml.nvmlDeviceGetCount()
-    print('deviceCount:',deviceCount)
+    print('deviceCount:', deviceCount)
     return deviceCount
+
 
 def showDown():
     pynvml.nvmlShutdown()
 
-def checkDeviceIsAvailable(count):
-    if count==0:
+
+def checkDeviceIsAvailable(gpuNum, appNum, msg):
+    if gpuNum == 0:
         return 0
-    for i in range(count):
+    gpuInfo = []
+    for i in range(gpuNum):
         handle = pynvml.nvmlDeviceGetHandleByIndex(i)
         # s = pynvml.nvmlDeviceGetMemoryInfo(handler,1)
         memoryInfo = pynvml.nvmlDeviceGetMemoryInfo(handle)
         processInfos = pynvml.nvmlDeviceGetGraphicsRunningProcesses_v2(handle)
         picInfo = pynvml.nvmlDeviceGetPciInfo(handle)
         # dis = pynvml.nvmlDeviceDiscoverGpus(picInfo)
-        if len(processInfos)>0:
-            print("显卡被占用,当前进程数量 count=",len(processInfos))
-        for m in memoryInfo:
-            print(m)
-        print(processInfos)
+        gpuInfo.append({'index': i, 'processCount': len(processInfos)})
+    freeGPU = 0
+    freeGPUIndex = []
+    if len(gpuInfo) > 0:
+        for g in gpuInfo:
+            if g['processCount'] == 0:
+                freeGPU += 1
+                freeGPUIndex.append(g["index"])
+    if freeGPU < appNum:
+        msg += "当前节点[" + NODE_NAME + "]存在进程冲突的可能"
+        return 2  # 进程冲突
 
-
-
-@app.route('/api/'+NODE_IP+'/checkNvidia', methods = ['GET'])
-def checkNvidiaByIp():
-    msg = ''
-    checkFlag = 0
     try:
-        deviceCount = deviceNvmlInit()
-        checkDeviceIsAvailable(deviceCount)
-    except Exception  as err:
-        msg+='初始化启动检测异常'
-        checkFlag = 0
-        print("检测异常：",err)
+        for index in freeGPUIndex:
+            # aR = torch.rand((10, 1024, 256)).to('cuda:'+index)
+            # b = (aR + 1) * 2
+            # print(aR.shape)
+            print("")
+    except RuntimeError as rE:
+        msg += str(rE)
+        if rE.message in "out of memory":
+            return 1  # 调用显卡OOM
+        else:
+            return 3  # 检测出现异常 调用GPU失败
+    return 0
+
+
+@app.route('/api/checkNvidia', methods=['GET'])
+def checkNvidia():
+    appNum = request.args.get("appNum")
+    if appNum == None:
+        appNum = 1
+    msg = []
+    flag = False
+    # 0 成功 1内存溢出无法使用 2 进程冲突 3 检测异常 4 没有GPU
+    status = 0
+    statusMsg = ['SUCCESS', 'OOM', 'PROCESS_CONFLICT', "CHECK_ERROR", "NO GPU", ""]
+    try:
+        count = deviceNvmlInit()  # 初始化ml
+        status = checkDeviceIsAvailable(count, int(appNum), msg)
+    except Exception as err:
+        flag = False
+        msg += str(err)
     finally:
         showDown()
+    return jsonify({'msg': ''.join(msg), 'flag': flag, 'status': status})
 
 
-
-    return jsonify("ok2")
-  
-@app.route('/api/'+NODE_NAME+'/checkNvidia', methods = ['GET'])
-def checkNvidiaByNodeName():
-    path = request.args.get('path')
+@app.route('/api/repair', methods=['GET'])
+def gpuRepair():
     return jsonify("ok")
 
+
 if __name__ == '__main__':
-    app.run(port=80, host='0.0.0.0',debug=True)
+    app.run(port=80, host='0.0.0.0', debug=True)
